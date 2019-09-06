c3c5a1037afbf67739985c9e6398f995aaa5caf5
Jennifer Silva <jennifersilva20@gmail.com>
2019-09-06 21:14:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -47,7 +47,9 @@ def add_s(array)
   array.each_with_index.collect do |word,index|
     if index != 1 
       word = word + "s"
-        else
-          word = word
-        end
+    else
+      word = word
+    end
+  end 
+end
   
